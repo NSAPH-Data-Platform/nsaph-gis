c6fdd3bf3f54a44dbadd4d79c7441e3b8b44774f
mmcentre <michael.bouzinier@kvitneset.com>
2023-04-18 21:50:28 -0400

    MAB           Fixing tiger shapefiles URLs

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as readme:
 
 setup(
     name='nsaph_gis',
-    version="0.0.4",
+    version="0.0.5",
     url='https://github.com/NSAPH-Data-Platform/nsaph-gis',
     license='',
     author='Harvard University and Quantori LLC',
